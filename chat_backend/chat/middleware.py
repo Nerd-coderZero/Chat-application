d7f4e38473ac2f16869381cdddf7ad6676e06a04
Kushagra Jaiswal <61771073+Nerd-coderZero@users.noreply.github.com>
2025-02-05 05:37:59 +0530

    Update middleware.py

--- chat_backend/chat/middleware.py
+++ chat_backend/chat/middleware.py
@@ -1,3 +1,4 @@
+# middleware.py
 from channels.middleware import BaseMiddleware
 from django.contrib.auth.models import AnonymousUser
 from channels.db import database_sync_to_async
@@ -17,7 +18,8 @@ class TokenAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         query_string = scope.get('query_string', b'').decode()
         params = parse_qs(query_string)
-        token_key = params.get('token', [None])[0]
+        token_key = params.get('token', [None])[0]  # Get token from query parameters
+
         if token_key:
             scope['user'] = await get_user(token_key)
         else:
